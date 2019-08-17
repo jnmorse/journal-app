8d80f505584d23f1c21d93b9f1e31723ea9fbfb6
Joseph Morse <tamed.lionheart@gmail.com>
2019-08-17 07:16:22 -0400

    chore: added current_user route

--- src/api-routes/user-routes.ts
+++ src/api-routes/user-routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
-import { requireLogin } from '../middleware/passport-middleware';
+import { requireLogin, requireAuth } from '../middleware/passport-middleware';
 import { signin, signup } from '../controllers/autherization';
 
 const userRoutes = Router();
 
 userRoutes.post('/signin', requireLogin, signin);
 userRoutes.post('/signup', signup);
+userRoutes.get('/current_user', requireAuth, (req, res) => {
+  res.send(req.user);
+});
 
 export { userRoutes };
--- src/middleware/passport-middleware.ts
+++ src/middleware/passport-middleware.ts
@@ -1,5 +1,17 @@
 import passport from 'passport';
+import { Request, Response, NextFunction } from 'express';
 
 import '../services/passport';
 
 export const requireLogin = passport.authenticate('local');
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  if (req.user) {
+    return next();
+  }
+
+  return next('Authentication required');
+};
